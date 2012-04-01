c3e99e3642d25b1d12fec07b2a70fce933516526
Lee McAlilly <leemcalilly@gmail.com>
2012-04-01 19:26:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derparty::Application.config.secret_token = 'ce96fb4be19b63024e543b3a37534ec493a2a25f58a73fa0c7e130166c664a82d342201d5d463830caf3ae860c4596502adf3c19decd8965fb5dc5f6a4292aca'
