e834589a116d99e9a7dc5c80ad9d2f813dde2a94
Lee McAlilly <leemcalilly@gmail.com>
2012-04-03 22:21:23 -0400

    seeing if heroku will work without this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  if Rails.env.production? do
-    before_filter :check_url
-  end
-
-  def check_url
-    redirect_to request.protocol + "www." + request.host_with_port + request.fullpath if !/^www/.match(request.host)
-  end
+ 
 end
