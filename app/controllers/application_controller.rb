2059b69ec013aad505d41686b4b0d9c1763d7c2f
Lee McAlilly <leemcalilly@gmail.com>
2012-04-03 21:59:05 -0400

    tweaking photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :check_uri
+  Rails.env.production? do
+    before_filter :check_url
+  end
 
-  def check_uri
+  def check_url
     redirect_to request.protocol + "www." + request.host_with_port + request.fullpath if !/^www/.match(request.host)
   end
 end
